8d26876bbd9ed5cb2eec20f49ee7ba8c925009b8
Muhammad Nurul Ahsan <86627956+Muhammadnurulahsan21@users.noreply.github.com>
2021-11-18 11:35:55 +0600

    new code

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const express = require("express");
 const app = express();
-const port = 4000;
 
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("Running my CRUD Server");
+const { MongoClient } = require("mongodb");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tchpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("best-travel");
+    const packagesCollection = database.collection("packages");
+    console.log("Travel Database Connection Successful");
+
+    // Get All PACKAGES Data
+    app.get("/packages", async (req, res) => {
+      const cursor = packagesCollection.find({});
+      const count = await cursor.count();
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let packages;
+      if (page) {
+        packages = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        packages = await cursor.toArray();
+      }
+      res.send({
+        count,
+        packages,
+      });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Best Travel!");
+});
 
 app.listen(port, () => {
-  console.log("Running Server on port", port);
+  console.log(`Running Server on ${port}`);
 });
